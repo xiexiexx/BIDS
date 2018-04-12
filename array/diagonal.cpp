4a9ae72c2b1154d6cac986a688e39d0615d413c5
Xie Xie <DSAD2015@163.com>
2018-04-12 13:11:53 +0800

    20180412

---
+++ array/diagonal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  const int n = 64;
+  int x = 4;
+  int y = 2;
+  int M[n][n];
+
+  // 方法1:
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      if (i == j)
+        M[i][j] = x;
+      else
+        M[i][j] = y;
+
+  // 方法2:
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      M[i][j] = y;
+  for (int i = 0; i < n; ++i)
+    M[i][i] = x;
+
+  // 方法3:
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < i; ++j)
+      M[i][j] = y;
+    M[i][i] = x;
+    for (int j = i + 1; j < n; ++j)
+      M[i][j] = y;
+  }
+
+  // 在屏幕上输出.
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+      cout << M[i][j];
+    cout << endl;
+  }
+  return 0;
+}
