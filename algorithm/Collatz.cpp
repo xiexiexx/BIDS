eaa1727bf10ddab7e63f10dd800abe00229732ca
Xie Xie <DSAD2015@163.com>
2018-05-19 08:02:27 +0800

    corrected

--- algorithm/Collatz.cpp
+++ algorithm/Collatz.cpp
@@ -18,31 +18,35 @@ size_t iterative_Collatz(size_t n)
 
 size_t memoized_Collatz(vector<size_t>& v, size_t n)
 {
-  // 前提条件是n > 0.
-  size_t L = 1;
+  if (n < 1)
+    return 0;
+  // 如果n不在向量v的下标范围之内, 先转换到合理范围之内并计算偏移L.
+  size_t L = 0;
   while (n >= v.size())
   {
     n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     ++L;
   }
+  // 对v[n]进行赋值, 注意此处n在向量v的下标范围之内, 直接递归加1赋值即可.
   if (v[n] == 0)
-    v[n] = memoized_Collatz(v, (n % 2 == 0) ? n / 2 : 3 * n + 1) + L;
-  return v[n];
+    v[n] = memoized_Collatz(v, (n % 2 == 0) ? n / 2 : 3 * n + 1) + 1;
+  // 返回值是原有的n对应的序列长度, 应加上偏移量L.
+  return v[n] + L;
 }
 
 int main()
 {
-  size_t n;
-  cin >> n;
-  // 利用迭代直接求解.
-  cout << iterative_Collatz(n) << endl;
   // 利用备忘录保存已算出的值, 适合多次求解.
   const size_t m = 10000;
   vector<size_t> v(m, 0);
   v[1] = 1;
-  if (n < 1)
-    cout << 0 << endl;
-  else
-    cout << memoized_Collatz(v, n) << endl;
+  // 测试迭代和备忘录计算结果是否一致, 测试范围为[1, max].
+  size_t max = 100000;
+  bool equal = true;
+  for (size_t n = 1; n <= max; ++n)
+    if (iterative_Collatz(n) != memoized_Collatz(v, n))
+      equal = false;
+  cout << (equal ? "相符" : "不符") << endl;
+
   return 0;
 }
