2027608e88ebea483622d62826eb07a25ad5311c
Xie Xie <DSAD2015@163.com>
2018-11-12 18:27:20 +0800

    20181111

---
+++ unordered_set/unordered_set_example.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+void print_buckets(const unordered_set<int>& S)
+{
+  for (size_t i = 0; i < S.bucket_count(); ++i)
+  {
+    cout << "Bucket " << i << ": ";
+    for (auto iter = S.begin(i); iter != S.end(i); ++iter)
+      cout << *iter << ' ';
+    cout << endl;
+  }
+}
+
+int main()
+{
+  vector<int> V = {7, 4, 3, -6, 5, 4, 22, 71, 42, -96, 81, 12, 105};
+  unordered_set<int> S(V.begin(), V.end());
+  S.insert(23);
+  S.erase(5);
+  print_buckets(S);
+  for (auto iter = S.begin(); iter != S.end(); ++iter)
+    cout << *iter << ' ';
+  cout << endl;
+  for (auto iter = S.begin(); iter != S.end(); ++iter)
+    if (*iter % 2 == 0)
+      iter = S.erase(iter);
+    else
+      ++iter;
+  for (const auto& x : S)
+    cout << x << ' ';
+  cout << endl;
+  print_buckets(S);
+  cout << "Max Load Factor: " << S.max_load_factor() << endl;
+  cout << "Load Factor: " << S.load_factor() << endl;
+  S.max_load_factor(0.5);
+  for (const auto& x : V)
+    S.insert(-x);
+  print_buckets(S);
+  cout << "Max Load Factor: " << S.max_load_factor() << endl;
+  cout << "Load Factor: " << S.load_factor() << endl;
+  return 0;
+}
