6cbe4c91eab1703cc70375d6822c72524c9d4985
Xie Xie <DSAD2015@163.com>
2019-02-11 01:08:59 +0800

    Create multiway_merge.cpp

---
+++ priority_queue/multiway_merge.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+void merge(const vector<vector<T>>& V, vector<T>& S)
+{
+  size_t L = 0;
+  for (const auto& X : V)
+    L += X.size();
+  S.clear();
+  S.reserve(L);
+  using range = pair<decltype(V[0].begin()), decltype(V[0].end())>;
+  auto cmp = [](range a, range b) { return *(a.first) > *(b.first); };
+  priority_queue<range, vector<range>, decltype(cmp)> PQ(cmp);
+  for (const auto& X : V)
+    if (X.begin() != X.end())
+      PQ.push({X.begin(), X.end()});
+  while (!PQ.empty())
+  {
+    auto R = PQ.top();
+    PQ.pop();
+    S.push_back(*(R.first++));
+    if (R.first != R.second)
+      PQ.push(R);
+  }
+}
+
+int main()
+{
+  vector<vector<int>> A = {{1, 2, 4}, {}, {2, 3, 5}, {3, 4, 6, 8}};
+  // A[0], A[1], A[2], A[3]均为有序向量.
+  vector<int> B;
+  merge(A, B);
+  for (const auto& x : B)
+    cout << x << endl;
+  return 0;
+}
