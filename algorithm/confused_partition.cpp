3dab4c4aa615a101aba546a7b7d99967e033691d
Xie Xie <DSAD2015@163.com>
2019-03-27 22:34:31 +0800

    Create confused_partition.cpp

---
+++ algorithm/confused_partition.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  vector<int> V = {2, 1, 3, 4, 5};
+  int min = *min_element(V.begin(), V.end());
+  auto M = partition(V.begin(), V.end(),
+                     [min](const auto& x) { return x < min; });
+  for (auto iter = V.begin(); iter != M; ++iter)
+    cout << *iter << " ";
+  cout << "* ";
+  for (auto iter = M; iter != V.end(); ++iter)
+    cout << *iter << " ";
+  cout << endl;
+  // result: * 2 1 3 4 5
+  int max = *max_element(V.begin(), V.end());
+  M = partition(V.begin(), V.end(),
+                [max](const auto& x) { return !(max < x); });
+  for (auto iter = V.begin(); iter != M; ++iter)
+    cout << *iter << " ";
+  cout << "* ";
+  for (auto iter = M; iter != V.end(); ++iter)
+    cout << *iter << " ";
+  cout << endl;
+  // result: 2 1 3 4 5 *
+  return 0;
+}
