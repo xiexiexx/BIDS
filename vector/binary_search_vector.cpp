0205b8fba25d234caa7712757e51a8a1b7cad8e3
Xie Xie <DSAD2015@163.com>
2019-04-22 19:51:35 +0800

    Update binary_search_vector.cpp

--- vector/binary_search_vector.cpp
+++ vector/binary_search_vector.cpp
@@ -3,8 +3,10 @@
 
 using namespace std;
 
+// 为了简便起见, 没有使用vector<T>::const_iterator.
 template <typename T>
-bool binary_search_vector(const T& key, typename vector<T>::const_iterator data, size_t N)
+bool binary_search_vector(const T& key, typename vector<T>::iterator data,
+                          size_t N)
 {
   size_t low = 0;
   size_t high = N;
@@ -21,12 +23,34 @@ bool binary_search_vector(const T& key, typename vector<T>::const_iterator data,
   return false;
 }
 
+// 使用迭代器, 描述更清晰, 而且还可支持数组.
+template <typename T, typename iterator>
+bool binary_search_iterator(const T& key, iterator L, iterator R)
+{
+  while (L < R)
+  {
+    iterator M = L + (R - L) / 2;
+    if (key < *M)       // 小则去前半部分继续查找.
+      R = M;
+    else if (*M < key)  // 大则去后半部分继续查找.
+      L = M + 1;
+    else
+      return true;
+  }
+  return false;
+
+}
+
 int main()
 {
   vector<int> v = {1, 2, 3, 4, 5};
-  cout << binary_search_vector(2, v.cbegin(), 5) << endl;
-  cout << binary_search_vector(0, v.cbegin(), 5) << endl;
-  cout << binary_search_vector(2, v.cbegin() + 2, 3) << endl;
-  cout << binary_search_vector(0, v.cbegin(), 0) << endl;
+  cout << binary_search_vector(2, v.begin(), 5) << endl;
+  cout << binary_search_vector(0, v.begin(), 5) << endl;
+  cout << binary_search_vector(2, v.begin() + 2, 3) << endl;
+  cout << binary_search_vector(0, v.begin(), 0) << endl;
+  cout << binary_search_iterator(2, v.begin(), v.end()) << endl;
+  cout << binary_search_iterator(0, v.begin(), v.end()) << endl;
+  cout << binary_search_iterator(2, v.begin() + 2, v.end()) << endl;
+  cout << binary_search_iterator(0, v.begin(), v.end()) << endl;
   return 0;
 }
