f9fee78359ba0fe63a5f897a1f556c11c37e0b83
Xie Xie <DSAD2015@163.com>
2019-05-16 13:26:56 +0800

    Update set_example.cpp

--- set/set_example.cpp
+++ set/set_example.cpp
@@ -7,7 +7,7 @@ int main()
 {
   set<int> S = {3, 2, 1, 4, 5};
 
-  // 放入.
+  // 插入.
   S.insert(1);
   S.insert(7);
 
@@ -18,6 +18,7 @@ int main()
   iter = S.find(5);
   if (iter != S.end())
     S.erase(iter);
+  S.erase(5);
 
   // 练习: 如何修改元素?
 
@@ -29,13 +30,16 @@ int main()
   --iter;
   cout << *iter << endl;
 
-  // 遍历集合.
-  for (auto iter = S.cbegin(); iter != S.cend(); ++iter)
-    cout << *iter << endl;
-  for (auto riter = S.crbegin(); riter != S.crend(); ++riter)
-    cout << *riter << endl;
+  // 遍历集合, 为简便计我们不讨论常量迭代器.
+  for (auto iter = S.begin(); iter != S.end(); ++iter)
+    cout << *iter << ' ';
+  cout << endl;
+  for (auto riter = S.rbegin(); riter != S.rend(); ++riter)
+    cout << *riter << ' ';
+  cout << endl;
   for (const auto& x : S)
-    cout << x << endl;
-
+    cout << x << ' ';
+  cout << endl;
+  
   return 0;
 }
