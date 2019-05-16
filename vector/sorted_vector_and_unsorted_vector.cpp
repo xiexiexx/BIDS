108d048bc304f51dfbc6846da10c927680067f7b
Xie Xie <DSAD2015@163.com>
2019-05-16 15:05:39 +0800

    Update sorted_vector_and_unsorted_vector.cpp

--- vector/sorted_vector_and_unsorted_vector.cpp
+++ vector/sorted_vector_and_unsorted_vector.cpp
@@ -4,52 +4,61 @@
 
 using namespace std;
 
+template <typename T>
+void print_all(const T& S)
+{
+  for (const auto& x : S)
+    cout << x << ' ';
+  cout << endl;
+}
+
 int main()
 {
   int key;
 
   // 有序向量.
-  vector<int> sv = {1, 3, 6, 6, 8, 9};
+  vector<int> SV = {1, 3, 6, 6, 8, 9};
+  // 查找元素.
+  cin >> key;
+  auto iter = lower_bound(SV.begin(), SV.end(), key);
+  cout << iter - SV.begin() << endl;
+  iter = upper_bound(SV.begin(), SV.end(), key);
+  cout << iter - SV.begin() << endl;
   // 插入新元素.
   key = 0;
-  auto iter_sv = lower_bound(sv.begin(), sv.end(), key);
-  sv.insert(iter_sv, key);
-  // 删除元素(重复元素的最后一个). 思考: 如何删除第一个?
+  SV.insert(upper_bound(SV.begin(), SV.end(), key), key);
+  // 删除重复元素的最后一个. 思考: 如何删除第一个?
   key = 6;
   // 如果存在则删除.
-  if (binary_search(sv.begin(), sv.end(), key))
+  if (binary_search(SV.begin(), SV.end(), key))
   {
-    iter_sv = upper_bound(sv.begin(), sv.end(), key);
-    --iter_sv;
-    sv.erase(iter_sv);
+    iter = upper_bound(SV.begin(), SV.end(), key);
+    SV.erase(--iter);
   }
   // 查找重复key所在的区间range, 区间为[range.first, range.second).
-  auto range = equal_range(sv.begin(), sv.end(), key);
+  auto range = equal_range(SV.begin(), SV.end(), key);
   // 删除整个range区间的元素.
-  sv.erase(range.first, range.second);
+  SV.erase(range.first, range.second);
   // 打印.
-  for (const auto& x : sv)
-    cout << x << " ";
-  cout << endl;
+  print_all(SV);
 
   // 无序向量.
-  vector<int> usv = {9, 6, 1, 3, 8, 6};
+  vector<int> USV = {9, 6, 1, 3, 8, 6};
   // 插入新元素.
   key = 0;
-  usv.push_back(key);
-  // 删除元素(重复元素的第一个). 思考: 如何删除最后一个?
+  USV.push_back(key);
+  // 删除重复元素的最后一个. 思考: 如何删除第一个?
   key = 6;
-  auto iter_usv = find(usv.begin(), usv.end(), key);
+  // 注意这里得使用逆向迭代器, 而不能使用正向迭代器iter.
+  auto riter = find(USV.rbegin(), USV.rend(), key);
   // 如果存在则删除.
-  if (iter_usv != usv.end())
+  if (riter != USV.rend())
   {
-    *iter_usv = usv.back();
-    usv.pop_back();
+    *riter = USV.back();
+    USV.pop_back();
   }
   // 打印.
-  for (auto& x : usv)
-    cout << x << " ";
-  cout << endl;
+  print_all(USV);
 
   return 0;
 }
