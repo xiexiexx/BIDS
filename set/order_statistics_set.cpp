38393f74c915f322c699b59b1b7797109a9fabe9
Xie Xie <DSAD2015@163.com>
2019-05-16 13:37:55 +0800

    Update order_statistics_set.cpp

--- set/order_statistics_set.cpp
+++ set/order_statistics_set.cpp
@@ -9,7 +9,7 @@ int main()
   set<int> S = {3, 2, 1, 4, 5};
   int k;
   cin >> k;
-  if (k <= 0 || k > S.size())
+  if (k < 1 || k > S.size())
     cout << "超出范围!" << endl;
   else
   {
@@ -18,7 +18,7 @@ int main()
     for (int i = 0; i < k - 1; ++i)
       ++iter;
     cout << *iter << endl;
-    // 第二种方法:
+    // 第二种方法(较慢):
     vector<int> V;
     // 预留k - 1个元素空间.
     V.reserve(k - 1);
@@ -32,6 +32,7 @@ int main()
     // 将前k - 1个元素放回S中.
     for (size_t i = 0; i < V.size(); ++i)
       S.insert(V[i]);
+    // 或者直接使用: S.insert(V.begin(), V.end());
   }
   return 0;
 }
