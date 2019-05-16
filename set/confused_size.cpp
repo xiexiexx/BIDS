874281213842712983d88da24f16522cc9ae7bae
Xie Xie <DSAD2015@163.com>
2019-05-16 14:25:57 +0800

    Create confused_size.cpp

---
+++ set/confused_size.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+// 基于迭代器打印容器中所有元素.
+template <typename iterator>
+void print_all(iterator L, iterator R)
+{
+  while (L != R)
+    cout << *L++ << ' ';
+  cout << endl;
+}
+
+// 基于范围的循环打印容器中所有元素.
+template <typename T>
+void print_all(const T& S)
+{
+  for (const auto& x : S)
+    cout << x << ' ';
+  cout << endl;
+}
+
+int main()
+{
+  set<int> S = {3, 1, 2, 4, 5};
+  print_all(S.begin(), S.end());
+  print_all(S);
+
+  // 初学者容易犯的错误:
+  for (size_t i = 0; i < S.size(); ++i)
+  {
+    cout << *S.begin() << ' ';
+    S.erase(S.begin());
+  }
+  cout << endl;
+  // 应该直接以size()函数为准:
+  while (S.size() > 0)  // !S.empty()
+  {
+    cout << *S.begin() << ' ';
+    S.erase(S.begin());
+  }
+  cout << endl;
+  // 最简单的做法:
+  S.clear();
+
+  return 0;
+}
