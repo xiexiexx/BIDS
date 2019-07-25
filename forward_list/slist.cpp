1b3ce7ac0185061ecb0d9509bc7c03f91d231cb2
Xie Xie <DSAD2015@163.com>
2019-07-25 14:26:27 +0800

    Update slist.cpp

--- forward_list/slist.cpp
+++ forward_list/slist.cpp
@@ -25,6 +25,17 @@ int main()
   for (p = header->next; p != NULL; p = p->next)
     cout << p->data << endl;
 
+  // 先将结点A之后的结点B删除.
+  p = &A;
+  snode* q = p->next;
+  p->next = q->next;
+  // 再将刚删除的结点B插入到结点C之后.
+  p = p->next;
+  q->next = p->next;
+  p->next = q;
+  for (p = header->next; p != NULL; p = p->next)
+    cout << p->data << endl;
+
   vector<snode> V = {{}, {1, NULL}, {2, NULL}, {3, NULL}};
   header = p = &V[0];
   for (size_t i = 1; i < V.size(); ++i)
