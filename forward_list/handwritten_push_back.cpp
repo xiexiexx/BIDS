80a263939ddcc19cf8a3e7dd1a67098433d71ef7
Xie Xie <DSAD2015@163.com>
2019-08-25 15:30:49 +0800

    Update handwritten_push_back.cpp

--- forward_list/handwritten_push_back.cpp
+++ forward_list/handwritten_push_back.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
@@ -35,5 +36,16 @@ int main()
     delete p;
     p = next;
   }
+
+  // 如果已知链的最终长度, 可以先用vector一次性申请内存以提速, 还可以避免手写代码释放内存.
+  start = clock();
+  vector<snode> L(n + 1);
+  header = &L[0];
+  for (size_t i = 0; i < n; ++i)
+    L[i].next = &L[i + 1];
+  L.back().next = NULL;
+  end = clock();
+  cout << "运行时间(s): " << time(start, end) << endl;
+
   return 0;
 }
