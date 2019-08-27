b980b8b01933e2393a8df03d4921938f0a3667d7
Xie Xie <DSAD2015@163.com>
2019-08-27 14:32:14 +0800

    Update maze_with_stack.cpp

--- stack/maze_with_stack.cpp
+++ stack/maze_with_stack.cpp
@@ -2,20 +2,25 @@
 #include <vector>
 using namespace std;
 
-struct status {
+// 为方便讲解, 我们不定义point的相等(==)和不相等(!=)运算符.
+struct point {
   int x;
   int y;
-  int direction;
+};
+
+struct status {
+  point pt;
+  int direction;  // 以direction取值0, 1, 2, 3标记东南西北.
 };
 
 const int d = 4;      // 可行方向总数.
+// 以下标取值0, 1, 2, 3标记东南西北与当前位置的偏移量.
+point delta[d] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-void find_next(status& current, status& next)
+void find_next(point& current, int direction, point& next)
 {
-  // 以direction取值0, 1, 2, 3标记东南西北.
-  pair<int, int> delta[d] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-  next.x = current.x + delta[current.direction].first;
-  next.y = current.y + delta[current.direction].second;
+  next.x = current.x + delta[direction].x;
+  next.y = current.y + delta[direction].y;
 }
 
 int main()
@@ -33,30 +38,29 @@ int main()
       {'*', '*', '*', '*', '*', '*', '*'}
   };
   // 起点与终点的状态.
-  status source = {1, 1, 0};
-  status destination = {3, 5, 0};
-  vector<status> path;                      // 搜索时保留路径所用的向量, 可视为栈.
-  path.reserve(M * N);                      // 提前预留容量.
-  path.push_back(source);                   // 初始点设为入口点.
-  while (!path.empty())
-    if (path.back().direction < d)
+  point source = {1, 1};
+  point destination = {3, 5};
+  vector<status> P;                         // 搜索时保留路径所用的向量, 可视为栈.
+  P.reserve(M * N);                         // 提前预留容量.
+  P.push_back({source, 0});                 // 初始点设为入口点, 并设定初始方向.
+  while (!P.empty())
+    if (P.back().direction < d)
     {
-      status next;
-      find_next(path.back(), next);
-      ++path.back().direction;
+      point next;
+      find_next(P.back().pt, P.back().direction, next);
+      ++P.back().direction;
       if (maze[next.x][next.y] == unvisited)
       {
         maze[next.x][next.y] = visited;
-        next.direction = 0;   // 设置next的初始方向.
-        path.push_back(next);
+        P.push_back({next, 0});
         // 放到路径向量后再判断该点是否为终点并及时跳出循环.
         if (next.x == destination.x && next.y == destination.y)
           break;
       }
     }
     else
-      path.pop_back();
-  for (size_t i = 0; i < path.size(); ++i)
-    cout << path[i].x << ' ' << path[i].y << endl;
+      P.pop_back();
+  for (size_t i = 0; i < P.size(); ++i)
+    cout << P[i].pt.x << ' ' << P[i].pt.y << endl;
   return 0;
 }
