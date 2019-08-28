13385ab6065dfd186b91e56163a59bb016be3842
Xie Xie <DSAD2015@163.com>
2019-08-28 18:23:17 +0800

    Update generalized_validator.cpp

--- stack/generalized_validator.cpp
+++ stack/generalized_validator.cpp
@@ -3,10 +3,10 @@
 #include <stack>
 using namespace std;
 
-// 4种状态常量以及对应提示语, 注意使用text时下标要加上偏移d.
-const int d = 4;
+// 4种状态常量以及对应提示语, 注意使用text时下标要加上偏移s.
+const int s = 4;
 enum status {not_matched = -4, matched = -3, illegal = -2, in = -1};
-string text[d] = {"不匹配", "匹配", "存在非法字符", ""};
+const string text[s] = {"不匹配", "匹配", "存在非法字符", ""};
 
 int generalized_validator(const string& brackets, int value[])
 {
@@ -29,14 +29,14 @@ int main()
   int value[n];
   for (size_t i = 0; i < n; ++i)
     value[i] = illegal;
-  value[(int)'('] = in;
-  value[(int)')'] = '(';
-  value[(int)'{'] = in;
-  value[(int)'}'] = '{';
+  value[(int)'L'] = in;
+  value[(int)'R'] = (int)'L';
+  value[(int)'<'] = in;
+  value[(int)'>'] = (int)'<';
   value[(int)'['] = in;
-  value[(int)']'] = '[';
+  value[(int)']'] = (int)'[';
   string brackets;
   cin >> brackets;
-  cout << text[d + generalized_validator(brackets, value)] << endl;
+  cout << text[s + generalized_validator(brackets, value)] << endl;
   return 0;
 }
