ae189ef0437dbd8cbae92459a9933315411be6d6
Xie Xie <DSAD2015@163.com>
2019-09-19 02:21:34 +0800

    Update rebuild.cpp

--- set/rebuild.cpp
+++ set/rebuild.cpp
@@ -11,22 +11,24 @@ struct tnode {
 };
 
 template <typename data_iterator, typename node_iterator>
-pair<node_iterator, bool> pre_in_build(data_iterator pre, data_iterator in,
-                                       node_iterator root, size_t n)
+bool pre_in_build(data_iterator pre, data_iterator in,
+                  node_iterator root, size_t n)
 {
   if (n == 0)
-    return {NULL, true};
+    return true;
   data_iterator pivot = find(in, in + n, *pre);
   if (pivot == in + n)
-    return {NULL, false};
-  size_t L = pivot - in;
-  size_t R = (in + n) - pivot - 1;
-  auto left_result = pre_in_build(pre + 1, in, root + 1, L);
-  auto right_result = pre_in_build(pre + 1 + L, in + 1 + L, root + 1 + L, R);
-  if (!left_result.second || !right_result.second)
-    return {NULL, false};
-  *root = {*pivot, left_result.first, right_result.first};
-  return {root, true};
+    return false;
+  size_t cl = pivot - in;
+  size_t cr = in + n - pivot - 1;
+  if (!pre_in_build(pre + 1, in, root + 1, cl))
+    return false;
+  root->left = (cl == 0) ? NULL : root + 1;
+  if (!pre_in_build(pre + 1 + cl, in + 1 + cl, root + 1 + cl, cr))
+    return false;
+  root->right = (cr == 0) ? NULL : root + 1 + cl;
+  root->data = *pivot;
+  return true;
 }
 
 template <typename node_iterator>
@@ -48,9 +50,8 @@ int main()
   if (n > 0 && in_order.size() == n)
   {
     vector<tnode<string>> tree(n);
-    auto result = pre_in_build(&pre_order[0], &in_order[0], &tree[0], n);
-    if (result.second)
-      post_order(result.first);
+    if (pre_in_build(&pre_order[0], &in_order[0], &tree[0], n))
+      post_order(&tree[0]);
   }
   return 0;
 }
