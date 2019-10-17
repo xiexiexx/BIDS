21b5ec16361a076f9eb6de88522dbfdf639cbcf6
Xie Xie <DSAD2015@163.com>
2019-10-17 11:41:52 +0800

    Update bool_vector_and_bitset.cpp

--- bitset/bool_vector_and_bitset.cpp
+++ bitset/bool_vector_and_bitset.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 int main()
 {
-  const size_t N = 60000000;
-  vector<bool> A(N, true);
-  bitset<N> B;
+  const size_t n = 60000000;
+  vector<bool> A(n, true);
+  bitset<n> B;
   B.flip();
-  double start_t, end_t;  // 计时器.
-  const size_t L = 100;
+  double start_t, end_t;
+  const size_t loop = 100;
   start_t = clock();
-  for (size_t i = 0; i < L; ++i)
+  for (size_t i = 0; i < loop; ++i)
     for (size_t j = 0; j < A.size(); ++j)
       if (j % 2 == 1)
         A[j] = false;
@@ -22,7 +22,7 @@ int main()
   std::cout << (end_t - start_t) / (CLOCKS_PER_SEC * 60)
             << " minutes" << std::endl;
   start_t = clock();
-  for (size_t i = 0; i < L; ++i)
+  for (size_t i = 0; i < loop; ++i)
     for (size_t j = 0; j < B.size(); ++j)
       if (j % 2 == 1)
         B[j] = false;
