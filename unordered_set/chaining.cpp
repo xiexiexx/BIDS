0f22d77186fdc94a9f67e1fd3a776873922df85b
Xie Xie <DSAD2015@163.com>
2019-10-17 11:13:35 +0800

    Update chaining.cpp

--- unordered_set/chaining.cpp
+++ unordered_set/chaining.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <list>
+#include <string>
 
 using namespace std;
 
@@ -10,24 +11,25 @@ void print_all(const T& H)
 {
   for (const auto& B : H)
   {
+    cout << "> ";
     for (const auto& x : B)
       cout << x << ' ';
     cout << endl;
   }
-  cout << "==================" << endl;
+  cout << string(80, '=') << endl;
 }
 
 int main()
 {
   vector<int> K = {89, -738, 2118, -439, 43289, 89, 67, 251};
-  size_t n = 2 * K.size();
-  vector<set<int>> S(n);
-  vector<vector<int>> V(n);
-  vector<list<int>> L(n);
+  size_t u = 2 * K.size();
+  vector<set<int>> S(u);
+  vector<vector<int>> V(u);
+  vector<list<int>> L(u);
   hash<int> h;
   for (const auto& x : K)
   {
-    size_t pos = h(x) % n;
+    size_t pos = h(x) % u;
     S[pos].insert(x);
     if (find(V[pos].begin(), V[pos].end(), x) == V[pos].end())
       V[pos].push_back(x);
@@ -38,12 +40,16 @@ int main()
   print_all(V);
   print_all(L);
   for (const auto& x : K)
+  for (size_t i = 0; i < 2; ++i)
   {
-    size_t pos = h(x) % n;
+    size_t pos = h(x) % u;
     S[pos].erase(x);
     auto iter = find(V[pos].begin(), V[pos].end(), x);
     if (iter != V[pos].end())
-      V[pos].erase(iter);
+    {
+      *iter = move(V[pos].back());
+      V[pos].pop_back();
+    }
     L[pos].remove(x);
   }
   return 0;
