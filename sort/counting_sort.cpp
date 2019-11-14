2d6003992bed1b95aaad250206c3e59ea1207a95
Xie Xie <DSAD2015@163.com>
2019-11-14 14:23:44 +0800

    Update counting_sort.cpp

--- sort/counting_sort.cpp
+++ sort/counting_sort.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-  // 取值范围为[0, K)的排序.
-  const int K = 42;
+  // 取值范围为[0, k)的排序.
+  const size_t k = 42;
   vector<size_t> V = {18, 2, 6, 15, 6, 32, 1, 25, 0, 27, 15};
   // 对每个元素进行计数并给出它们最终排序后对范围.
-  vector<size_t> C(K + 1, 0);
+  vector<size_t> C(k + 1, 0);
   for (const auto& x : V)
     ++C[x + 1];
   for (size_t i = 1; i < C.size(); ++i)
