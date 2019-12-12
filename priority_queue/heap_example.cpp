fabadb4de9d3c16c3888768c330f98fe107babdd
Xie Xie <DSAD2015@163.com>
2019-12-13 00:13:47 +0800

    Update heap_example.cpp

--- priority_queue/heap_example.cpp
+++ priority_queue/heap_example.cpp
@@ -26,7 +26,7 @@ int main()
   A.push_back(9);
   push_heap(A.begin(), A.end());
   cout << A.front() << endl;
-  // 利用pop_heap完成堆排序, 最大堆最后变成从小到大按升序排练的向量.
+  // 利用pop_heap完成堆排序, 最大堆最后变成从小到大按升序排列的向量.
   auto B = V;
   make_heap(B.begin(), B.end());
   for (size_t d = B.size(); d > 1; --d)
@@ -34,7 +34,7 @@ int main()
     pop_heap(B.begin(), B.begin() + d);
     print_all(B);
   }
-  // 利用sort_heap排序, 最小堆最后变成从大到小按降序排练的向量, 注意加入了判断准则.
+  // 利用sort_heap排序, 最小堆最后变成从大到小按降序排列的向量, 注意加入了判断准则.
   auto C = V;
   make_heap(C.begin(), C.end(), greater<int>());
   sort_heap(C.begin(), C.end(), greater<int>());
