85f183bccffd1f4c61021f21016e349495408213
Xie Xie <DSAD2015@163.com>
2019-12-23 00:48:38 +0800

    Update linear_search.cpp

--- algorithm/linear_search.cpp
+++ algorithm/linear_search.cpp
@@ -5,66 +5,63 @@
 using namespace std;
 
 template <typename T>
-int linear_search_array(const T& key, const T data[], int N)
+int linear_search_array(const T& key, const T data[], int n)
 {
-  for (int pos = 0; pos < N; ++pos)
-    if (data[pos] == key)
-      return pos;
-  return N;
+  for (int i = 0; i < n; ++i)
+    if (data[i] == key)
+      return i;
+  return n;
 }
 
-// 要求data的长度大于N.
+// 要求data的长度大于n.
 template <typename T>
-int linear_search_array_sentinel(const T& key, T data[], int N)
+int linear_search_array_sentinel(const T& key, T data[], int n)
 {
-  data[N] = key;
-  int pos = 0;
-  while (data[pos] != key)
-      ++pos;
-  return pos;
+  data[n] = key;
+  int i = 0;
+  while (data[i] != key)
+      ++i;
+  return i;
 }
 
 template <typename T>
 size_t linear_search_vector(const T& key, const vector<T>& data)
 {
-  for (size_t pos = 0; pos < data.size(); ++pos)
-    if (data[pos] == key)
-      return pos;
+  for (size_t i = 0; i < data.size(); ++i)
+    if (data[i] == key)
+      return i;
   return data.size();
 }
 
 template <typename T, typename iterator>
-iterator linear_search_iterator(const T& key, iterator L, iterator R)
+iterator linear_search_iterator(const T& key, iterator left, iterator right)
 {
-  while (L != R && *L != key)
-    ++L;
-  return L;
+  while (left != right && *left != key)
+    ++left;
+  return left;
 }
 
-// 原始写法有点冗长.
 /*
-  while (L != R)
-  {
-    if (*L == key)
-      return L;
-    ++L;
-  }
-  return L;
-*/
-// 另外, 不要过度追求简洁而出错, 从而返回不正确的迭代器位置.
-/*
-  if (*L++ == key)
-    return L;
-*/
+ * 原始写法有点冗长.
+ * while (left != right)
+ * {
+ *   if (*left == key)
+ *     return left;
+ *   ++left;
+ * }
+ * return left;
+ * 另外, 不要过度追求简洁而出错, 从而返回不正确的迭代器位置.
+ * if (*left++ == key)
+ *   return left;
+ */
 
 int main()
 {
-  const int N = 5;
-  const int L = N + 1;
-  int A[L] = {3, 2, 1, 4, 5, 0};
+  const int n = 5;
+  int A[n + 1] = {3, 2, 1, 4, 5, 0};
   vector<string> V = {"RSA", "Apple", "WWW"};
-  cout << linear_search_array(1, A, N) << endl;
-  cout << linear_search_array_sentinel(9, A, N) << endl;
+  cout << linear_search_array(1, A, n) << endl;
+  cout << linear_search_array_sentinel(9, A, n) << endl;
   string key = "Apple";
   cout << linear_search_vector(key, V) << endl;
   key = "Algorithm";
