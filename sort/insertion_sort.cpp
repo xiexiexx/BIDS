05e83a474ed42171b820b161782796b1a02f1182
Xie Xie <DSAD2015@163.com>
2020-04-14 16:31:51 +0800

    Update insertion_sort.cpp

--- sort/insertion_sort.cpp
+++ sort/insertion_sort.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <algorithm>
 
 using namespace std;
 
 template <typename T>
-void insertion_sort(vector<T>& V)
+void insertion_sort_vector(vector<T>& V)
 {
   for (size_t i = 1; i < V.size(); ++i)
   {
@@ -17,11 +18,28 @@ void insertion_sort(vector<T>& V)
   }
 }
 
+template <typename T>
+void insertion_sort_list(list<T>& L)
+{
+  for (auto iter = L.begin(); iter != L.end(); ++iter)
+  {
+    auto position = L.begin();
+    while (position != iter && *position < *iter)
+      ++position;
+    L.splice(position, L, iter);
+  }
+}
+
 int main()
 {
   vector<int> V = {2, 1, 3, 4, 5};
-  insertion_sort(V);
+  list<int> L(V.begin(), V.end());
+  insertion_sort_vector(V);
   for (const auto& x : V)
+    cout << x << ' ';
+  cout << endl;
+  insertion_sort_list(L);
+  for (const auto& x : L)
     cout << x << ' ';
   cout << endl;
   return 0;
