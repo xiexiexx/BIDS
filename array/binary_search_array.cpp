cad29af85b63cceea4e139a5be17085514980f73
Xie Xie <DSAD2015@163.com>
2021-01-02 22:44:59 +0800

    Update binary_search_array.cpp

--- array/binary_search_array.cpp
+++ array/binary_search_array.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
+// 输入: key - 待查值; data - 有序数据集(从小到大排列); n - 数据集长度.
+// 输出: 所找到的位置.
 template <typename T>
-int binary_search_array(const T& key, const T data[], int N)
+int binary_search_array(const T& key, const T data[], int n)
 {
-  if (N < 0)
+  if (n < 0)
     return -1;
   int low = 0;
-  int high = N - 1;
+  int high = n - 1;
   while (low <= high)
   {
     int mid = low + (high - low) / 2;
-    if (key < data[mid])		// 小则去前半部分继续查找.
+    if (key < data[mid])      // 小则去前半部分继续查找.
       high = mid - 1;
-    else if (data[mid] < key)	// 大则去后半部分继续查找.
+    else if (data[mid] < key) // 大则去后半部分继续查找.
       low = mid + 1;
     else
       return mid;
   }
-  return -1;
+  return n;
 }
 
 int main()
