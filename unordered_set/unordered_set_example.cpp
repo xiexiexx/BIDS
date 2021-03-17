72ecb28fca363174caba4fe98974f43744012114
Xie Xie <DSAD2015@163.com>
2021-03-17 20:35:45 +0800

    Update unordered_set_example.cpp

--- unordered_set/unordered_set_example.cpp
+++ unordered_set/unordered_set_example.cpp
@@ -2,14 +2,19 @@
 #include <vector>
 #include <unordered_set>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::unordered_set;
 
 template <typename T>
 void print_buckets(const unordered_set<T>& S)
 {
+  // 逐个打印桶内所存放的数据.
   for (size_t i = 0; i < S.bucket_count(); ++i)
   {
     cout << "Bucket " << i << ": ";
+    // 桶i的迭代器区间为[begin(i), end(i)).
     for (auto iter = S.begin(i); iter != S.end(i); ++iter)
       cout << *iter << ' ';
     cout << endl;
@@ -23,6 +28,7 @@ int main()
   S.insert(23);
   S.erase(5);
   print_buckets(S);
+  // 以迭代器遍历并打印S中所有元素, 从逻辑上可以认为是无序的. 
   for (auto iter = S.begin(); iter != S.end(); ++iter)
     cout << *iter << ' ';
   cout << endl;
@@ -36,8 +42,10 @@ int main()
     cout << x << ' ';
   cout << endl;
   print_buckets(S);
+  // 打印S的最大装填因子和装填因子当前值.
   cout << "Max Load Factor: " << S.max_load_factor() << endl;
   cout << "Load Factor: " << S.load_factor() << endl;
+  // 将S的最大装填因子设为0.5, 注意该操作可能会引发重散列.
   S.max_load_factor(0.5);
   for (const auto& x : V)
     S.insert(-x);
