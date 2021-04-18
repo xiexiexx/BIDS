b63a0bf2e5a90bfb07fecfdd11575fe00a80c12d
Xie Xie <DSAD2015@163.com>
2021-04-18 20:17:20 +0800

    Update unordered_set_example.cpp

--- unordered_set/unordered_set_example.cpp
+++ unordered_set/unordered_set_example.cpp
@@ -7,10 +7,20 @@ using std::endl;
 using std::vector;
 using std::unordered_set;
 
+// 注意到本例中两次打印迭代器区间中的所有元素, 不妨实现print_all函数.
+// 如果换成print_all调用可让代码更简洁, 具体修改作为练习.
+template <typename IR>
+void print_all(IR left, IR right)
+{
+  while (left != right)
+    cout << *left++ << ' ';
+  cout << endl;
+}
+
+// 逐个打印S中每个桶内所存放的数据.
 template <typename T>
 void print_buckets(const unordered_set<T>& S)
 {
-  // 逐个打印桶内所存放的数据.
   for (size_t i = 0; i < S.bucket_count(); ++i)
   {
     cout << "Bucket " << i << ": ";
@@ -28,7 +38,7 @@ int main()
   S.insert(23);
   S.erase(5);
   print_buckets(S);
-  // 以迭代器遍历并打印S中所有元素, 从逻辑上可以认为是无序的. 
+  // 以迭代器遍历并打印S中所有元素, 从逻辑上可以认为S是无序的.
   for (auto iter = S.begin(); iter != S.end(); ++iter)
     cout << *iter << ' ';
   cout << endl;
@@ -42,7 +52,7 @@ int main()
     cout << x << ' ';
   cout << endl;
   print_buckets(S);
-  // 打印S的最大装填因子和装填因子当前值.
+  // 打印S的最大装填因子和装填因子.
   cout << "Max Load Factor: " << S.max_load_factor() << endl;
   cout << "Load Factor: " << S.load_factor() << endl;
   // 将S的最大装填因子设为0.5, 注意该操作可能会引发重散列.
