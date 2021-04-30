5ea3d40f4599c1a19bcf8d41a0d09b1da4eb4749
Xie Xie <DSAD2015@163.com>
2021-04-30 16:32:38 +0800

    Update maze_with_stack.cpp

--- stack/maze_with_stack.cpp
+++ stack/maze_with_stack.cpp
@@ -5,23 +5,27 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-// 为方便讲解, 我们不定义point的相等(==)和不等(!=)运算符.
+// 定义(二维平面)点结构体, 包含其x和y坐标值,
+// 并以默认形式给出point的相等(==)运算符.
 struct point {
   int x;
   int y;
+  bool operator==(const point&) const = default;
 };
 
+// 状态包括点和方向, 不妨以direction取值0, 1, 2, 3标记东南西北.
 struct status {
   point pt;
-  int direction;  // 以direction取值0, 1, 2, 3标记东南西北.
+  int direction;
 };
 
 int main()
 {
   const int m = 5;
   const int n = 7;
-  const char unvisited = '0'; // 未访问过的标记.
-  const char visited = 'V';   // 访问过的标记.
+  // 未访问则标记为'0', 已访问则标记为'V'.
+  const char unvisited = '0';
+  const char visited = 'V';
   // 迷宫字符数组, 周围一圈全是墙(以'*'标记).
   char maze[m][n] = {
       {'*', '*', '*', '*', '*', '*', '*'},
@@ -30,28 +34,33 @@ int main()
       {'*', '0', '0', '0', '*', '0', '*'},
       {'*', '*', '*', '*', '*', '*', '*'}
   };
-  const int d = 4;            // 可行方向总数.
+  // 可行方向总数为d.
+  const int d = 4;
   // 以下标取值0, 1, 2, 3标记东南西北与当前位置的偏移量.
   const point delta[d] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
   // 起点与终点的坐标.
   point source = {1, 1};
-  point destination = {3, 5};
-  vector<status> P;           // 搜索时保留路径所用的向量, 可视为栈.
-  P.reserve(m * n);           // 提前预留容量.
-  P.push_back({source, 0});   // 初始点设为入口点, 并设定初始方向.
+  point destination = {m - 2, n - 2};
+  // 搜索时保留路径所用的向量P可视为栈.
+  vector<status> P;
+  // 提前预留容量, 以迷宫元素的总数为界.
+  P.reserve(m * n);
+  // 初始点设为入口点, 并设定初始方向.
+  P.push_back({source, 0});
   maze[source.x][source.y] = visited;
   while (!P.empty())
     if (P.back().direction < d)
     {
       point next = {P.back().pt.x + delta[P.back().direction].x,
                     P.back().pt.y + delta[P.back().direction].y};
+      // 调整下次将指示的方向.
       ++P.back().direction;
       if (maze[next.x][next.y] == unvisited)
       {
         maze[next.x][next.y] = visited;
         P.push_back({next, 0});
         // 放到路径向量后再判断该点是否为终点并及时跳出循环.
-        if (next.x == destination.x && next.y == destination.y)
+        if (next == destination)
           break;
       }
     }
