cd5278a157b6ebcd0de9639b9acf52cea9d1baf0
Xie Xie <DSAD2015@163.com>
2022-06-08 18:17:41 +0800

    Update phone.cpp

--- queue/phone.cpp
+++ queue/phone.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
 #include <vector>
 #include <string>
 #include <queue>
-using namespace std;
+#include <random>
+#include <algorithm>
+
+// 由于程序涉及多个容器的嵌套, 使用相关名字便于初学者阅读.
+using std::vector;
+using std::string;
+using std::queue;
 
 int main()
 {
@@ -11,6 +16,11 @@ int main()
   // 如果是特别长的字符串, 可以考虑用list<string>型链表存储原始数据.
   // 不要用字符串向量再配上字符串指针向量来变动操作, 那样效果不好.
   vector<string> phone_numbers(10000000, "123456789");
+  // 对每个电话号码字符串进行洗牌, 打乱其中数字.
+  std::random_device rd;
+  std::knuth_b g(rd());
+  for (auto& x : phone_numbers)
+    std::shuffle(x.begin(), x.end(), g);
   // 如果处理很长的字符串, 可将队列换为链表, 也即vector<list<string>>型,
   // 所对应的入队出队操作换为链表的splice操作, 这样更快.
   vector<queue<string>> Q(B);
@@ -22,6 +32,7 @@ int main()
     for (const auto& x : phone_numbers)
       Q[x[d] - '0'].push(x);
     size_t i = 0;
+    // 也可写成for (auto& q : Q)的形式, 更为简洁.
     for (size_t k = 0; k < B; ++k)
       while (!Q[k].empty())
       {
